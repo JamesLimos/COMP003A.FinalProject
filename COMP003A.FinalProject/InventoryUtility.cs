7de7fb33e266083a8feb9402c77296fed6f6074f
JamesLimos <limosjamesfrancis@gmail.com>
2025-02-27 09:08:29 -0800

    Added Validation for Name and Quantity

---
+++ COMP003A.FinalProject/InventoryUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.FinalProject
+{
+    public class InventoryUtility
+    {
+        public static void DescribeItem(string name)
+        {
+            Console.WriteLine($"Item Name: {name}");
+        }
+    }
+}
