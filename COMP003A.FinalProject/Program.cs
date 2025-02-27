baa31e1bd2590e31c5078826511d76bb4d888d69
JamesLimos <limosjamesfrancis@gmail.com>
2025-02-27 09:34:03 -0800

    Created the Menu

--- COMP003A.FinalProject/Program.cs
+++ COMP003A.FinalProject/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Item> items = new List<Item>();
+
+            while (true) 
+            {
+                Console.WriteLine("Welcome to Inventory management system: ");
+                Console.WriteLine("Please choice an option: ");
+                Console.WriteLine("1. Add a Perishable Food.");
+                Console.WriteLine("2. Add a Canned Food.");
+                Console.WriteLine("3. Display Inventory.");
+                Console.WriteLine("4. Update Quanity.");
+                Console.WriteLine("5. Delete an Item.");
+                Console.WriteLine("6. Exit");
+                Console.WriteLine("Your Choice: ");
+                int choice = int.Parse(Console.ReadLine());
         }
     }
 }
