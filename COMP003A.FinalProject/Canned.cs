8a5f50d1a00a341e13e57f60a063e74ee4e6def9
JamesLimos <limosjamesfrancis@gmail.com>
2025-02-27 09:24:01 -0800

    Made the canned derived class

---
+++ COMP003A.FinalProject/Canned.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.FinalProject
+{
+    internal class Canned : Item
+    {
+        public Canned(string name, int quantity) 
+        {
+            Name = name;
+            Quantity = quantity;
+        }
+
+        public override void GetDetails()
+        {
+          
+        }
+    }
+}
